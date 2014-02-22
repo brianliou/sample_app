06e57ecddccf75c3cc8e256d5d5bfc846731d965
Brian Wayne Liou <brian.liou91@gmail.com>
2014-02-22 11:15:04 -0800

    finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Redefining the cost factor in test environment for faster tests
+  ActiveModel::SecurePassword.min_cost = true
 end
